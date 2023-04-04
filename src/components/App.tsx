3efac0814d5aa424cd9e4a58be490d0df23e2a70
Paul Miller <paul@paul.lol>
2023-04-04 13:14:13 -0500

    add node manager

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For } from "solid-js";
+import { For } from "solid-js";
 import { A } from "solid-start";
 import { Motion, Presence } from "@motionone/solid";
 
@@ -7,6 +7,7 @@ import mutiny_m from '~/assets/icons/m.svg';
 import scan from '~/assets/icons/scan.svg';
 import settings from '~/assets/icons/settings.svg';
 import send from '~/assets/icons/send.svg';
+import BalanceBox from "./BalanceBox";
 
 // TODO: use this reload prompt for real
 // import ReloadPrompt from "./Reload";
@@ -26,7 +27,6 @@ function ActivityItem() {
 }
 
 export default function App() {
-    const [_isOpen, setOpen] = createSignal(false);
     return (
         <div class="safe-top safe-left safe-right safe-bottom">
             <div class="disable-scrollbars max-h-screen h-full overflow-y-scroll">
@@ -35,30 +35,7 @@ export default function App() {
                         <img src={logo} class="App-logo" alt="logo" />
                     </header>
                     {/* <ReloadPrompt /> */}
-                    <Presence>
-                        <Motion
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.5, easing: [0.87, 0, 0.13, 1] }}
-                        >
-                            <div class='border border-white rounded-xl border-b-4 p-4 flex flex-col gap-2'>
-                                <header class='text-sm font-semibold uppercase'>
-                                    Balance
-                                </header>
-                                <div>
-                                    <h1 class='text-4xl font-light'>
-                                        69,420 <span class='text-xl'>SAT</span>
-                                    </h1>
-                                </div>
-                                <div class="flex gap-2 py-4">
-                                    <button onClick={() => setOpen(true)} class='bg-[#1EA67F] p-4 flex-1 rounded-xl text-xl font-semibold '><span class="drop-shadow-sm shadow-black">Send</span></button>
-                                    <button class='bg-[#3B6CCC] p-4 flex-1 rounded-xl text-xl font-semibold '><span class="drop-shadow-sm shadow-black">Receive</span></button>
-                                </div>
-                            </div>
-                        </Motion>
-                    </Presence>
-
+                    <BalanceBox />
                     <div class='rounded-xl p-4 flex flex-col gap-2 bg-[rgba(0,0,0,0.5)]'>
                         <header class='text-sm font-semibold uppercase'>
                             Activity
@@ -103,7 +80,5 @@ export default function App() {
                 </nav>
             </div>
         </div >
-
-
     );
 }
