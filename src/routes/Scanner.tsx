67a7d08ad979164bacab6debfa300a866ee275c1
Paul Miller <paul@paul.lol>
2023-04-10 17:55:51 -0500

    oops pink

--- src/routes/Scanner.tsx
+++ src/routes/Scanner.tsx
@@ -27,7 +27,7 @@ export default function Scanner() {
                     </div>
                 </div>
             </Show>
-            <div class="w-full flex flex-col items-center fixed bottom-[2rem] gap-8 px-8 bg-pink-500">
+            <div class="w-full flex flex-col items-center fixed bottom-[2rem] gap-8 px-8">
                 <Show when={scanResult()}
                     fallback={
                         <div class="w-full max-w-[800px] flex flex-col gap-2">
