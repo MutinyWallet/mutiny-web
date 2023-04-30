cb348ad86911b97caf7940e6d88dda8d708b7301
Paul Miller <paul@paul.lol>
2023-04-29 23:25:12 -0500

    redshift logo and wave

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,11 @@ import mutiny_m from '~/assets/icons/m.svg';
 import airplane from '~/assets/icons/airplane.svg';
 import settings from '~/assets/icons/settings.svg';
 import receive from '~/assets/icons/big-receive.svg';
+import redshift from '~/assets/icons/rs.svg';
 
 import { A } from "solid-start";
 
-type ActiveTab = 'home' | 'scan' | 'send' | 'receive' | 'settings' | 'none';
+type ActiveTab = 'home' | 'scan' | 'send' | 'receive' | 'settings' | 'redshift' | 'none';
 
 export default function NavBar(props: { activeTab: ActiveTab }) {
     const activeStyle = 'border-t-0 border-b-0 p-2 bg-black rounded-lg'
@@ -31,6 +32,11 @@ export default function NavBar(props: { activeTab: ActiveTab }) {
                 <li class={props.activeTab === "settings" ? activeStyle : inactiveStyle}>
                     <A href="/settings">
                         <img src={settings} alt="settings" />
+                    </A>
+                </li>
+                <li class={props.activeTab === "redshift" ? activeStyle : inactiveStyle}>
+                    <A href="/redshift">
+                        <img src={redshift} alt="redshift" width={36} />
                     </A>
                 </li>
             </ul>
