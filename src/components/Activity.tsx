20b2c0bd8f42367e0606a209534e4060129bca17
Paul Miller <paul@paul.lol>
2023-05-03 16:40:20 -0500

    combined activity

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -1,6 +1,6 @@
 import send from '~/assets/icons/send.svg';
 import receive from '~/assets/icons/receive.svg';
-import { Card, LoadingSpinner, SmallAmount, SmallHeader, VStack } from './layout';
+import { Card, LoadingSpinner, NiceP, SmallAmount, SmallHeader, VStack } from './layout';
 import { For, Match, ParentComponent, Suspense, Switch, createMemo, createResource, createSignal } from 'solid-js';
 import { useMegaStore } from '~/state/megaStore';
 import { MutinyInvoice } from '@mutinywallet/mutiny-wasm';
@@ -11,7 +11,7 @@ import mempoolTxUrl from '~/utils/mempoolTxUrl';
 export const THREE_COLUMNS = 'grid grid-cols-[auto,1fr,auto] gap-4 py-2 px-2 border-b border-neutral-800 last:border-b-0'
 export const CENTER_COLUMN = 'min-w-0 overflow-hidden max-w-full'
 export const MISSING_LABEL = 'py-1 px-2 bg-white/10 rounded inline-block text-sm'
-export const RIGHT_COLUMN = 'flex flex-col items-right text-right max-w-[8rem]'
+export const RIGHT_COLUMN = 'flex flex-col items-right text-right max-w-[9rem]'
 
 export type OnChainTx = {
     txid: string
@@ -53,15 +53,17 @@ function OnChainItem(props: { item: OnChainTx }) {
                 </a>
             </JsonModal>
             <div class={THREE_COLUMNS} onClick={() => setOpen(!open())}>
-                {isReceive() ? <img src={receive} alt="receive arrow" /> : <img src={send} alt="send arrow" />}
+                <div class="flex items-center">
+                    {isReceive() ? <img src={receive} alt="receive arrow" /> : <img src={send} alt="send arrow" />}
+                </div>
                 <div class={CENTER_COLUMN}>
                     <h2 class={MISSING_LABEL}>Unknown</h2>
-                    {isReceive() ? <SmallAmount amount={props.item.received} /> : <SmallAmount amount={props.item.sent} />}
+                    {isReceive() ? <SmallAmount amount={props.item.received} sign="+" /> : <SmallAmount amount={props.item.sent} />}
                     {/* <h2 class="truncate">Txid: {props.item.txid}</h2> */}
                 </div>
                 <div class={RIGHT_COLUMN}>
-                    <SmallHeader class={isReceive() ? "text-m-green" : "text-m-red"}>
-                        {isReceive() ? "RECEIVE" : "SEND"}
+                    <SmallHeader>
+                        <span class="text-neutral-500">On-chain</span>&nbsp;{isReceive() ? <span class="text-m-green">Receive</span> : <span class="text-m-red">Send</span>}
                     </SmallHeader>
                     <SubtleText>{props.item.confirmation_time?.Confirmed ? prettyPrintTime(props.item.confirmation_time?.Confirmed?.time) : "Unconfirmed"}</SubtleText>
                 </div>
@@ -79,14 +81,16 @@ function InvoiceItem(props: { item: MutinyInvoice }) {
         <>
             <JsonModal open={open()} data={props.item} title="Lightning Transaction" setOpen={setOpen} />
             <div class={THREE_COLUMNS} onClick={() => setOpen(!open())}>
-                {isSend() ? <img src={send} alt="send arrow" /> : <img src={receive} alt="receive arrow" />}
+                <div class="flex items-center">
+                    {isSend() ? <img src={send} alt="send arrow" /> : <img src={receive} alt="receive arrow" />}
+                </div>
                 <div class={CENTER_COLUMN}>
                     <h2 class={MISSING_LABEL}>Unknown</h2>
                     <SmallAmount amount={props.item.amount_sats || 0} />
                 </div>
                 <div class={RIGHT_COLUMN}>
-                    <SmallHeader class={isSend() ? "text-m-red" : "text-m-green"}>
-                        {isSend() ? "SEND" : "RECEIVE"}
+                    <SmallHeader>
+                        <span class="text-neutral-500">Lightning</span>&nbsp;{!isSend() ? <span class="text-m-green">Receive</span> : <span class="text-m-red">Send</span>}
                     </SmallHeader>
                     <SubtleText>{prettyPrintTime(Number(props.item.expire))}</SubtleText>
                 </div>
@@ -201,5 +205,68 @@ export function Activity() {
             </Suspense>
         </VStack>
     )
+
+}
+
+type ActivityItem = { type: "onchain" | "lightning", item: OnChainTx | MutinyInvoice, time: number }
+
+function sortByTime(a: ActivityItem, b: ActivityItem) {
+    return b.time - a.time;
+}
+
+export function CombinedActivity(props: { limit?: number }) {
+
+    const [state, _] = useMegaStore();
+
+    const getAllActivity = async () => {
+        console.log("Getting all activity");
+        const txs = await state.node_manager?.list_onchain() as OnChainTx[];
+        const invoices = await state.node_manager?.list_invoices() as MutinyInvoice[];
+
+        let activity: ActivityItem[] = [];
+
+        txs.forEach((tx) => {
+            activity.push({ type: "onchain", item: tx, time: tx.confirmation_time?.Confirmed?.time || Date.now() })
+        })
+
+        invoices.forEach((invoice) => {
+            activity.push({ type: "lightning", item: invoice, time: Number(invoice.expire) })
+        })
+
+        if (props.limit) {
+            return activity.sort(sortByTime).slice(0, props.limit);
+        } else {
+            return activity.sort(sortByTime);
+        }
+    }
+
+    const [activity] = createResource(getAllActivity);
+
+    return (
+        <Switch>
+            <Match when={activity.loading}>
+                <LoadingSpinner wide />
+            </Match>
+            <Match when={activity.state === "ready" && activity().length === 0}>
+                <NiceP>No activity to show</NiceP>
+            </Match>
+            <Match when={activity.state === "ready" && activity().length >= 0}>
+                <For each={activity()}>
+                    {(activityItem) =>
+                        <Switch>
+                            <Match when={activityItem.type === "onchain"}>
+                                <OnChainItem item={activityItem.item as OnChainTx} />
+                            </Match>
+                            <Match when={activityItem.type === "lightning"}>
+                                <InvoiceItem item={activityItem.item as MutinyInvoice} />
+                            </Match>
+                        </Switch>
+                    }
+                </For>
+            </Match>
+        </Switch>
+
+    )
+
 
 }
