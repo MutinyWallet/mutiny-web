fd78cf05e751b25f7161fbd7612fd98021355ee5
Paul Miller <paul@paul.lol>
2023-05-04 15:55:11 -0500

    classy success / fail icons

--- src/routes/Receive.tsx
+++ src/routes/Receive.tsx
@@ -8,8 +8,6 @@ import { useMegaStore } from "~/state/megaStore";
 import { objectToSearchParams } from "~/utils/objectToSearchParams";
 import { useCopy } from "~/utils/useCopy";
 import mempoolTxUrl from "~/utils/mempoolTxUrl";
-
-import party from '~/assets/hands/handsup.png';
 import { Amount } from "~/components/Amount";
 import { FullscreenModal } from "~/components/layout/FullscreenModal";
 import { BackButton } from "~/components/layout/BackButton";
@@ -17,6 +15,7 @@ import { TagEditor, TagItem } from "~/components/TagEditor";
 import { StyledRadioGroup } from "~/components/layout/Radio";
 import { showToast } from "~/components/Toaster";
 import { useNavigate } from "solid-start";
+import megacheck from "~/assets/icons/megacheck.png";
 
 type OnChainTx = {
     transaction: {
@@ -272,7 +271,7 @@ export default function Receive() {
                                 onConfirm={() => { clearAll(); navigate("/"); }}
                             >
                                 <div class="flex flex-col items-center gap-8">
-                                    <img src={party} alt="party" class="w-1/2 mx-auto max-w-[50vh] aspect-square" />
+                                    <img src={megacheck} alt="success" class="w-1/2 mx-auto max-w-[50vh] aspect-square" />
                                     <Amount amountSats={paymentInvoice()?.amount_sats} showFiat />
                                 </div>
                             </FullscreenModal>
@@ -285,7 +284,7 @@ export default function Receive() {
                                 onConfirm={() => { clearAll(); navigate("/"); }}
                             >
                                 <div class="flex flex-col items-center gap-8">
-                                    <img src={party} alt="party" class="w-1/2 mx-auto max-w-[50vh] aspect-square" />
+                                    <img src={megacheck} alt="success" class="w-1/2 mx-auto max-w-[50vh] aspect-square" />
                                     <Amount amountSats={paymentTx()?.received} showFiat />
                                     <a href={mempoolTxUrl(paymentTx()?.txid, "signet")} target="_blank" rel="noreferrer">
                                         Mempool Link
--- src/routes/Send.tsx
+++ src/routes/Send.tsx
@@ -12,8 +12,8 @@ import { ParsedParams, toParsedParams } from "./Scanner";
 import { showToast } from "~/components/Toaster";
 import eify from "~/utils/eify";
 import { FullscreenModal } from "~/components/layout/FullscreenModal";
-import handshake from "~/assets/hands/handshake.png";
-import thumbsdown from "~/assets/hands/thumbsdown.png";
+import megacheck from "~/assets/icons/megacheck.png"
+import megaex from "~/assets/icons/megaex.png";
 import mempoolTxUrl from "~/utils/mempoolTxUrl";
 import { BackButton } from "~/components/layout/BackButton";
 import { useNavigate } from "solid-start";
@@ -211,11 +211,11 @@ export default function Send() {
                         <div class="flex flex-col items-center gap-8 h-full">
                             <Switch>
                                 <Match when={sentDetails()?.failure_reason}>
-                                    <img src={thumbsdown} alt="thumbs down" class="w-1/2 mx-auto max-w-[50vh]" />
+                                    <img src={megaex} alt="fail" class="w-1/2 mx-auto max-w-[50vh]" />
                                     <p class="text-xl font-light py-2 px-4 rounded-xl bg-white/10">{sentDetails()?.failure_reason}</p>
                                 </Match>
                                 <Match when={true}>
-                                    <img src={handshake} alt="handshake" class="w-1/2 mx-auto max-w-[50vh]" />
+                                    <img src={megacheck} alt="success" class="w-1/2 mx-auto max-w-[50vh]" />
                                     <Amount amountSats={sentDetails()?.amount} showFiat />
                                     <Show when={sentDetails()?.txid}>
                                         <a href={mempoolTxUrl(sentDetails()?.txid, state.node_manager?.get_network())} target="_blank" rel="noreferrer">
