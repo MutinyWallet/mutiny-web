449fb57dc331ecaf081947d8239d50092677570e
Paul Miller <paul@paul.lol>
2023-05-24 08:34:18 -0500

    feedback link

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,29 +14,46 @@ export default function App() {
     const [state, _actions] = useMegaStore();
 
     return (
-        <SafeArea>
-            <DefaultMain>
-                <header class="w-full flex justify-between items-center mt-4 mb-2">
-                    <img src={logo} class="h-10" alt="logo" />
-                    <A class="md:hidden p-2 hover:bg-white/5 rounded-lg active:bg-m-blue" href="/activity"><img src={userClock} alt="Activity" class="h-8 w-8" /></A>
-                </header>
-                <Show when={!state.wallet_loading}>
-                    <OnboardWarning />
-                    <ReloadPrompt />
-                </Show>
-                <BalanceBox loading={state.wallet_loading} />
-                <Card title="Activity">
-                    <div class="p-1" />
-                    <VStack>
-                        <Show when={!state.wallet_loading} fallback={<LoadingShimmer />}>
-                            <CombinedActivity limit={3} />
-                        </Show>
-                        {/* <ButtonLink href="/activity">View All</ButtonLink> */}
-                    </VStack>
-                    <A href="/activity" class="text-m-red active:text-m-red/80 text-xl font-semibold no-underline self-center">View All</A>
-                </Card>
-            </DefaultMain>
-            <NavBar activeTab="home" />
-        </SafeArea>
+      <SafeArea>
+        <DefaultMain>
+          <header class="w-full flex justify-between items-center mt-4 mb-2">
+            <img src={logo} class="h-10" alt="logo" />
+            <A class="md:hidden p-2 hover:bg-white/5 rounded-lg active:bg-m-blue" href="/activity">
+              <img src={userClock} alt="Activity" class="h-8 w-8" />
+            </A>
+          </header>
+          <Show when={!state.wallet_loading}>
+            <OnboardWarning />
+            <ReloadPrompt />
+          </Show>
+          <BalanceBox loading={state.wallet_loading} />
+          <Card title="Activity">
+            <div class="p-1" />
+            <VStack>
+              <Show when={!state.wallet_loading} fallback={<LoadingShimmer />}>
+                <CombinedActivity limit={3} />
+              </Show>
+              {/* <ButtonLink href="/activity">View All</ButtonLink> */}
+            </VStack>
+            <A
+              href="/activity"
+              class="text-m-red active:text-m-red/80 text-xl font-semibold no-underline self-center"
+            >
+              View All
+            </A>
+          </Card>
+          <p class="self-center text-neutral-500 mt-4">
+            Bugs? Feedback?{" "}
+            <a
+              class="text-neutral-400"
+              target="_blank"
+              href="https://github.com/MutinyWallet/mutiny-web/issues"
+            >
+              Create an issue
+            </a>
+          </p>
+        </DefaultMain>
+        <NavBar activeTab="home" />
+      </SafeArea>
     );
 }
