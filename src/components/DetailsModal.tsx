769f922e7a753cdb797d2a8f9f4d9d1db287b9c7
Paul Miller <paul@paul.lol>
2023-05-24 09:15:47 -0500

    channel open details view

--- src/components/DetailsModal.tsx
+++ src/components/DetailsModal.tsx
@@ -7,45 +7,47 @@ import {
   Show,
   Switch,
   createMemo,
-} from "solid-js"
-import { Hr, ModalCloseButton, TinyButton, VStack } from "~/components/layout"
-import { MutinyInvoice } from "@mutinywallet/mutiny-wasm"
-import { OnChainTx } from "./Activity"
+  createResource
+} from "solid-js";
+import { Hr, ModalCloseButton, TinyButton, VStack } from "~/components/layout";
+import { MutinyChannel, MutinyInvoice } from "@mutinywallet/mutiny-wasm";
+import { OnChainTx } from "./Activity";
 
-import bolt from "~/assets/icons/bolt-black.svg"
-import chain from "~/assets/icons/chain-black.svg"
-import copyIcon from "~/assets/icons/copy.svg"
+import bolt from "~/assets/icons/bolt-black.svg";
+import chain from "~/assets/icons/chain-black.svg";
+import shuffle from "~/assets/icons/shuffle-black.svg";
+import copyIcon from "~/assets/icons/copy.svg";
 
-import { ActivityAmount } from "./ActivityItem"
-import { CopyButton } from "./ShareCard"
-import { prettyPrintTime } from "~/utils/prettyPrintTime"
-import { useMegaStore } from "~/state/megaStore"
-import { tagToMutinyTag } from "~/utils/tags"
-import { useCopy } from "~/utils/useCopy"
-import mempoolTxUrl from "~/utils/mempoolTxUrl"
-import { Network } from "~/logic/mutinyWalletSetup"
-import { AmountSmall } from "./Amount"
+import { ActivityAmount } from "./ActivityItem";
+import { CopyButton } from "./ShareCard";
+import { prettyPrintTime } from "~/utils/prettyPrintTime";
+import { useMegaStore } from "~/state/megaStore";
+import { tagToMutinyTag } from "~/utils/tags";
+import { useCopy } from "~/utils/useCopy";
+import mempoolTxUrl from "~/utils/mempoolTxUrl";
+import { Network } from "~/logic/mutinyWalletSetup";
+import { AmountSmall } from "./Amount";
 
-export const OVERLAY = "fixed inset-0 z-50 bg-black/50 backdrop-blur-sm"
-export const DIALOG_POSITIONER = "fixed inset-0 z-50 flex items-center justify-center"
+export const OVERLAY = "fixed inset-0 z-50 bg-black/50 backdrop-blur-sm";
+export const DIALOG_POSITIONER = "fixed inset-0 z-50 flex items-center justify-center";
 export const DIALOG_CONTENT =
-  "max-w-[500px] w-[90vw] max-h-[100dvh] overflow-y-scroll disable-scrollbars mx-4 p-4 bg-neutral-800/80 backdrop-blur-md shadow-xl rounded-xl border border-white/10"
+  "max-w-[500px] w-[90vw] max-h-[100dvh] overflow-y-scroll disable-scrollbars mx-4 p-4 bg-neutral-800/80 backdrop-blur-md shadow-xl rounded-xl border border-white/10";
 
 function LightningHeader(props: { info: MutinyInvoice }) {
-  const [state, _actions] = useMegaStore()
+  const [state, _actions] = useMegaStore();
 
   const tags = createMemo(() => {
     if (props.info.labels.length) {
-      const contact = state.mutiny_wallet?.get_contact(props.info.labels[0])
+      const contact = state.mutiny_wallet?.get_contact(props.info.labels[0]);
       if (contact) {
-        return [tagToMutinyTag(contact)]
+        return [tagToMutinyTag(contact)];
       } else {
-        return []
+        return [];
       }
     } else {
-      return []
+      return [];
     }
-  })
+  });
 
   return (
     <div class="flex flex-col items-center gap-4">
@@ -63,70 +65,90 @@ function LightningHeader(props: { info: MutinyInvoice }) {
       />
       <For each={tags()}>
         {(tag) => (
-          <TinyButton tag={tag} onClick={() => {
-            // noop
-          }}>
+          <TinyButton
+            tag={tag}
+            onClick={() => {
+              // noop
+            }}
+          >
             {tag.name}
           </TinyButton>
         )}
       </For>
     </div>
-  )
+  );
 }
 
 function OnchainHeader(props: { info: OnChainTx }) {
-  const [state, _actions] = useMegaStore()
+  const [state, _actions] = useMegaStore();
+
+  const getChannelInfo = async () => {
+    const isChannel = props.info.labels.find((label) => label.startsWith("LN Channel"));
+
+    if (isChannel) {
+      const channels = await (state.mutiny_wallet?.list_channels() as Promise<MutinyChannel[]>);
+      const thisChannel = channels.find((channel) => channel.outpoint?.startsWith(props.info.txid));
+      return thisChannel;
+    }
+  };
+
+  const [channelInfo] = createResource(getChannelInfo);
 
   const tags = createMemo(() => {
     if (props.info.labels.length) {
-      const contact = state.mutiny_wallet?.get_contact(props.info.labels[0])
+      const contact = state.mutiny_wallet?.get_contact(props.info.labels[0]);
       if (contact) {
-        return [tagToMutinyTag(contact)]
+        return [tagToMutinyTag(contact)];
       } else {
-        return []
+        return [];
       }
     } else {
-      return []
+      return [];
     }
-  })
+  });
 
   const isSend = () => {
-    return props.info.sent > props.info.received
-  }
+    return props.info.sent > props.info.received;
+  };
 
   const amount = () => {
     if (isSend()) {
-      return (props.info.sent - props.info.received).toString()
+      return (props.info.sent - props.info.received).toString();
     } else {
-      return (props.info.received - props.info.sent).toString()
+      return (props.info.received - props.info.sent).toString();
     }
-  }
+  };
 
   return (
     <div class="flex flex-col items-center gap-4">
       <div class="p-4 bg-neutral-100 rounded-full">
-        <img src={chain} alt="blockchain" class="w-8 h-8" />
+        <Switch>
+          <Match when={channelInfo()}>
+            <img src={shuffle} alt="swap" class="w-8 h-8" />
+          </Match>
+          <Match when={true}>
+            <img src={chain} alt="blockchain" class="w-8 h-8" />
+          </Match>
+        </Switch>
       </div>
       <h1 class="uppercase font-semibold">
-        {isSend() ? "On-chain send" : "On-chain receive"}
+        {channelInfo() ? "Channel open" : isSend() ? "On-chain send" : "On-chain receive"}
       </h1>
-      <ActivityAmount
-        center
-        amount={amount() ?? "0"}
-        price={state.price}
-        positive={!isSend()}
-      />
+      <ActivityAmount center amount={amount() ?? "0"} price={state.price} positive={!isSend()} />
       <For each={tags()}>
         {(tag) => (
-          <TinyButton tag={tag} onClick={() => {
-            // noop
-          }}>
+          <TinyButton
+            tag={tag}
+            onClick={() => {
+              // noop
+            }}
+          >
             {tag.name}
           </TinyButton>
         )}
       </For>
     </div>
-  )
+  );
 }
 
 const KeyValue: ParentComponent<{ key: string }> = (props) => {
@@ -135,11 +157,11 @@ const KeyValue: ParentComponent<{ key: string }> = (props) => {
       <span class="uppercase font-semibold whitespace-nowrap">{props.key}</span>
       <span class="font-light">{props.children}</span>
     </li>
-  )
-}
+  );
+};
 
 function MiniStringShower(props: { text: string }) {
-  const [copy, _copied] = useCopy({ copiedTimeout: 1000 })
+  const [copy, _copied] = useCopy({ copiedTimeout: 1000 });
 
   return (
     <div class="w-full grid gap-1 grid-cols-[minmax(0,_1fr)_auto]">
@@ -148,7 +170,7 @@ function MiniStringShower(props: { text: string }) {
         <img src={copyIcon} alt="copy" class="w-4 h-4" />
       </button>
     </div>
-  )
+  );
 }
 
 function LightningDetails(props: { info: MutinyInvoice }) {
@@ -156,20 +178,14 @@ function LightningDetails(props: { info: MutinyInvoice }) {
     <VStack>
       <ul class="flex flex-col gap-4">
         <KeyValue key="Status">
-          <span class="text-neutral-300">
-            {props.info.paid ? "Paid" : "Unpaid"}
-          </span>
+          <span class="text-neutral-300">{props.info.paid ? "Paid" : "Unpaid"}</span>
         </KeyValue>
         <KeyValue key="When">
-          <span class="text-neutral-300">
-            {prettyPrintTime(Number(props.info.last_updated))}
-          </span>
+          <span class="text-neutral-300">{prettyPrintTime(Number(props.info.last_updated))}</span>
         </KeyValue>
         <Show when={props.info.description}>
           <KeyValue key="Description">
-            <span class="text-neutral-300 truncate">
-              {props.info.description}
-            </span>
+            <span class="text-neutral-300 truncate">{props.info.description}</span>
           </KeyValue>
         </Show>
         <KeyValue key="Fees">
@@ -188,32 +204,40 @@ function LightningDetails(props: { info: MutinyInvoice }) {
         </KeyValue>
       </ul>
     </VStack>
-  )
+  );
 }
 
 function OnchainDetails(props: { info: OnChainTx }) {
-  const [state, _actions] = useMegaStore()
+  const [state, _actions] = useMegaStore();
 
   const confirmationTime = () => {
-    return props.info.confirmation_time?.Confirmed?.time
-  }
+    return props.info.confirmation_time?.Confirmed?.time;
+  };
 
-  const network = state.mutiny_wallet?.get_network() as Network
+  const getChannelInfo = async () => {
+    const isChannel = props.info.labels.find((label) => label.startsWith("LN Channel"));
+
+    if (isChannel) {
+      const channels = await (state.mutiny_wallet?.list_channels() as Promise<MutinyChannel[]>);
+      const thisChannel = channels.find((channel) => channel.outpoint?.startsWith(props.info.txid));
+      return thisChannel;
+    }
+  };
+
+  const [channelInfo] = createResource(getChannelInfo);
+
+  const network = state.mutiny_wallet?.get_network() as Network;
 
   return (
     <VStack>
       <ul class="flex flex-col gap-4">
         <KeyValue key="Status">
-          <span class="text-neutral-300">
-            {confirmationTime() ? "Confirmed" : "Unconfirmed"}
-          </span>
+          <span class="text-neutral-300">{confirmationTime() ? "Confirmed" : "Unconfirmed"}</span>
         </KeyValue>
         <Show when={confirmationTime()}>
           <KeyValue key="When">
             <span class="text-neutral-300">
-              {confirmationTime()
-                ? prettyPrintTime(Number(confirmationTime()))
-                : "Pending"}
+              {confirmationTime() ? prettyPrintTime(Number(confirmationTime())) : "Pending"}
             </span>
           </KeyValue>
         </Show>
@@ -222,6 +246,23 @@ function OnchainDetails(props: { info: OnChainTx }) {
             <AmountSmall amountSats={props.info.fee} />
           </span>
         </KeyValue>
+        <Show when={channelInfo()}>
+          <KeyValue key="Reserve">
+            <span class="text-neutral-300">
+              <AmountSmall amountSats={channelInfo()?.reserve} />
+            </span>
+          </KeyValue>
+          <KeyValue key="Balance">
+            <span class="text-neutral-300">
+              <AmountSmall amountSats={channelInfo()?.balance} />
+            </span>
+          </KeyValue>
+          <KeyValue key="Peer">
+            <span class="text-neutral-300">
+              <MiniStringShower text={channelInfo()?.peer ?? ""} />
+            </span>
+          </KeyValue>
+        </Show>
         <KeyValue key="Txid">
           <MiniStringShower text={props.info.txid ?? ""} />
         </KeyValue>
@@ -235,7 +276,7 @@ function OnchainDetails(props: { info: OnChainTx }) {
         Mempool.space
       </a>
     </VStack>
-  )
+  );
 }
 
 export function DetailsModal(props: {
