7aea08c36c7b7787ef10b66c6859bc923bf26db0
Paul Miller <paul@paul.lol>
2023-06-29 14:24:18 -0500

    multi nwc

--- src/routes/settings/Connections.tsx
+++ src/routes/settings/Connections.tsx
@@ -1,39 +1,171 @@
-import { Show, createMemo } from "solid-js";
+import { NwcProfile } from "@mutinywallet/mutiny-wasm";
+import { For, Show, createResource, createSignal } from "solid-js";
 import { QRCodeSVG } from "solid-qr-code";
+import { KeyValue, MiniStringShower } from "~/components/DetailsModal";
+import { InfoBox } from "~/components/InfoBox";
 import NavBar from "~/components/NavBar";
 import { ShareCard } from "~/components/ShareCard";
 import {
     Button,
+    Collapser,
     DefaultMain,
     LargeHeader,
     MutinyWalletGuard,
     NiceP,
-    SafeArea
+    SafeArea,
+    SettingsCard,
+    SimpleDialog,
+    VStack
 } from "~/components/layout";
 import { BackLink } from "~/components/layout/BackLink";
+import { TextField } from "~/components/layout/TextField";
 import { useMegaStore } from "~/state/megaStore";
+import eify from "~/utils/eify";
 
-export default function Connections() {
-    const [state, actions] = useMegaStore();
+function Nwc() {
+    const [state, _actions] = useMegaStore();
 
-    const connectionURI = createMemo(() => {
-        if (state.nwc_enabled) {
-            return state.mutiny_wallet?.get_nwc_uri();
+    const [nwcProfiles, { refetch }] = createResource(async () => {
+        try {
+            const profiles: NwcProfile[] =
+                await state.mutiny_wallet?.get_nwc_profiles();
+
+            console.log("profiles:", profiles);
+
+            return profiles;
+        } catch (e) {
+            console.error(e);
         }
     });
 
-    const toggleNwc = async () => {
-        if (state.nwc_enabled) {
-            actions.setNwc(false);
-            window.location.reload();
-        } else {
-            actions.setNwc(true);
-            const nodes = await state.mutiny_wallet?.list_nodes();
-            const firstNode = (nodes[0] as string) || "";
-            await state.mutiny_wallet?.start_nostr_wallet_connect(firstNode);
-        }
-    };
+    const [dialogOpen, setDialogOpen] = createSignal(false);
+    const [formName, setFormName] = createSignal("");
+    const [createLoading, setCreateLoading] = createSignal(false);
+    const [error, setError] = createSignal("");
 
+    async function createConnection() {
+        try {
+            setError("");
+            setCreateLoading(true);
+
+            if (formName() === "") {
+                setError("Name cannot be empty");
+                return;
+            }
+            const profile = await state.mutiny_wallet?.create_nwc_profile(
+                formName(),
+                10000n
+            );
+
+            if (!profile) {
+                setError("Failed to create Wallet Connection");
+                return;
+            } else {
+                refetch();
+            }
+
+            setFormName("");
+            setDialogOpen(false);
+        } catch (e) {
+            setError(eify(e).message);
+            console.error(e);
+        } finally {
+            setCreateLoading(false);
+        }
+    }
+
+    async function toggleEnabled(profile: NwcProfile) {
+        try {
+            await state.mutiny_wallet?.edit_nwc_profile({
+                ...profile,
+                enabled: !profile.enabled
+            });
+            refetch();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    // this is because the TextField component has a bunch of required props
+    function noop() {
+        // do nothing
+        return;
+    }
+
+    return (
+        <VStack biggap>
+            <Button intent="blue" onClick={() => setDialogOpen(true)}>
+                Add Connection
+            </Button>
+            <Show when={nwcProfiles() && nwcProfiles()!.length > 0}>
+                <SettingsCard title="Manage Connections">
+                    <For each={nwcProfiles()}>
+                        {(profile) => (
+                            <Collapser
+                                title={profile.name}
+                                activityLight={profile.enabled ? "on" : "off"}
+                            >
+                                <VStack>
+                                    <KeyValue key="Relay">
+                                        <MiniStringShower
+                                            text={profile.relay}
+                                        />
+                                    </KeyValue>
+
+                                    <div class="w-full bg-white rounded-xl">
+                                        <QRCodeSVG
+                                            value={profile.nwc_uri}
+                                            class="w-full h-full p-8 max-h-[320px]"
+                                        />
+                                    </div>
+                                    <ShareCard text={profile.nwc_uri || ""} />
+                                    <Button
+                                        layout="small"
+                                        onClick={() => toggleEnabled(profile)}
+                                    >
+                                        {profile.enabled ? "Disable" : "Enable"}
+                                    </Button>
+                                </VStack>
+                            </Collapser>
+                        )}
+                    </For>
+                </SettingsCard>
+            </Show>
+            <SimpleDialog
+                open={dialogOpen()}
+                setOpen={setDialogOpen}
+                title="New Connection"
+            >
+                <div class="flex flex-col gap-4 py-4">
+                    <TextField
+                        name="name"
+                        label="Name"
+                        ref={noop}
+                        value={formName()}
+                        onInput={(e) => setFormName(e.currentTarget.value)}
+                        error={""}
+                        onBlur={noop}
+                        onChange={noop}
+                        placeholder="My favorite nostr client..."
+                    />
+                    <Show when={error()}>
+                        <InfoBox accent="red">{error()}</InfoBox>
+                    </Show>
+                </div>
+
+                <Button
+                    intent="blue"
+                    loading={createLoading()}
+                    onClick={createConnection}
+                >
+                    Create Connection
+                </Button>
+            </SimpleDialog>
+        </VStack>
+    );
+}
+
+export default function Connections() {
     return (
         <MutinyWalletGuard>
             <SafeArea>
@@ -41,24 +173,10 @@ export default function Connections() {
                     <BackLink href="/settings" title="Settings" />
                     <LargeHeader>Wallet Connections</LargeHeader>
                     <NiceP>
-                        Authorize Mutiny Wallet with external services like
-                        Nostr clients.
+                        Authorize external services to request payments from
+                        your wallet. Pairs great with Nostr clients.
                     </NiceP>
-
-                    <Button onClick={toggleNwc}>
-                        {state.nwc_enabled
-                            ? "Disable Nostr Wallet Connect"
-                            : "Enable Nostr Wallet Connect"}
-                    </Button>
-                    <Show when={connectionURI() && state.nwc_enabled}>
-                        <div class="w-full bg-white rounded-xl">
-                            <QRCodeSVG
-                                value={connectionURI() || ""}
-                                class="w-full h-full p-8 max-h-[400px]"
-                            />
-                        </div>
-                        <ShareCard text={connectionURI() || ""} />
-                    </Show>
+                    <Nwc />
                     <div class="h-full" />
                 </DefaultMain>
                 <NavBar activeTab="settings" />
