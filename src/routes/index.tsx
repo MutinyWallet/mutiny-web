dfb8699f7bd1c12ee1e5a33c492d191608c1be73
Paul Miller <paul@paul.lol>
2023-07-11 16:37:56 -0500

    remove waitlist

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,5 @@
 import App from "~/components/App";
 import { Switch, Match } from "solid-js";
-import { WaitlistAlreadyIn } from "~/components/waitlist/WaitlistAlreadyIn";
-import WaitlistForm from "~/components/waitlist/WaitlistForm";
 import { useMegaStore } from "~/state/megaStore";
 import { FullscreenLoader } from "~/components/layout";
 import SetupErrorDisplay from "~/components/SetupErrorDisplay";
@@ -14,14 +12,8 @@ export default function Home() {
             <Match when={state.setup_error}>
                 <SetupErrorDisplay initialError={state.setup_error!} />
             </Match>
-            <Match when={state.user_status === "approved"}>
+            <Match when={true}>
                 <App />
-            </Match>
-            <Match when={state.user_status === "waitlisted"}>
-                <WaitlistAlreadyIn />
-            </Match>
-            <Match when={state.user_status === "new_here"}>
-                <WaitlistForm />
             </Match>
         </Switch>
     );
