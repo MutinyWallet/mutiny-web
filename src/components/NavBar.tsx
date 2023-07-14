03688025b5802e555a4e2e933147989f313ed378
Paul Miller <paul@paul.lol>
2023-07-14 16:39:35 -0500

    fix navbar item padding

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -17,74 +17,68 @@ type ActiveTab =
     | "activity"
     | "none";
 
+function NavBarItem(props: {
+    href: string;
+    icon: string;
+    active: boolean;
+    alt: string;
+}) {
+    return (
+        <li>
+            <A
+                class="block rounded-lg p-2"
+                href={props.href}
+                classList={{
+                    "hover:bg-white/5 active:bg-m-blue": !props.active,
+                    "bg-black": props.active
+                }}
+            >
+                <img src={props.icon} alt={props.alt} height={36} width={36} />
+            </A>
+        </li>
+    );
+}
+
 export default function NavBar(props: { activeTab: ActiveTab }) {
-    const activeStyle = "border-t-0 border-b-0 p-2 bg-black rounded-lg";
-    const inactiveStyle = "p-2 hover:bg-white/5 rounded-lg active:bg-m-blue";
     return (
         <nav class="hidden md:block fixed shadow-none z-40 safe-bottom top-0 bottom-auto left-0 h-full">
-            <ul class="h-16 flex flex-col justify-start gap-4 px-4 mt-4">
-                <li
-                    class={
-                        props.activeTab === "home" ? activeStyle : inactiveStyle
-                    }
-                >
-                    <A href="/">
-                        <img src={mutiny_m} alt="home" />
-                    </A>
-                </li>
-                <li
-                    class={
-                        props.activeTab === "send" ? activeStyle : inactiveStyle
-                    }
-                >
-                    <A href="/send">
-                        <img src={airplane} alt="send" />
-                    </A>
-                </li>
-                <li
-                    class={
-                        props.activeTab === "receive"
-                            ? activeStyle
-                            : inactiveStyle
-                    }
-                >
-                    <A href="/receive">
-                        <img src={receive} alt="receive" />
-                    </A>
-                </li>
-                <li
-                    class={
-                        props.activeTab === "activity"
-                            ? activeStyle
-                            : inactiveStyle
-                    }
-                >
-                    <A href="/activity">
-                        <img src={userClock} alt="activity" />
-                    </A>
-                </li>
-                <li
-                    class={
-                        props.activeTab === "redshift"
-                            ? activeStyle
-                            : inactiveStyle
-                    }
-                >
-                    <A href="/redshift">
-                        <img src={redshift} alt="redshift" width={36} />
-                    </A>
-                </li>
-                <li
-                    class={
-                        props.activeTab === "settings"
-                            ? activeStyle
-                            : inactiveStyle
-                    }
-                >
-                    <A href="/settings">
-                        <img src={settings} alt="Settings" />
-                    </A>
-                </li>
+            <ul class="flex flex-col justify-start gap-4 px-4 mt-4">
+                <NavBarItem
+                    href="/"
+                    icon={mutiny_m}
+                    active={props.activeTab === "home"}
+                    alt="home"
+                />
+                <NavBarItem
+                    href="/send"
+                    icon={airplane}
+                    active={props.activeTab === "send"}
+                    alt="send"
+                />
+                <NavBarItem
+                    href="/receive"
+                    icon={receive}
+                    active={props.activeTab === "receive"}
+                    alt="receive"
+                />
+                <NavBarItem
+                    href="/activity"
+                    icon={userClock}
+                    active={props.activeTab === "activity"}
+                    alt="activity"
+                />
+                <NavBarItem
+                    href="/redshift"
+                    icon={redshift}
+                    active={props.activeTab === "redshift"}
+                    alt="redshift"
+                />
+                <NavBarItem
+                    href="/settings"
+                    icon={settings}
+                    active={props.activeTab === "settings"}
+                    alt="settings"
+                />
             </ul>
         </nav>
     );
