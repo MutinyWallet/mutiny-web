6f3a83f6e5ee6db71ad0abdb895b182ece2cbe32
gawlk <kevin@gawlik.io>
2023-08-18 18:38:31 +0200

    prettier improvements

---
+++ prettier.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Options} */
+export default {
+  trailingComma: "none",
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  printWidth: 80,
+  useTabs: false,
+
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss", // MUST come last
+  ],
+
+  tailwindFunctions: ["classList"],
+
+  importOrder: ["<THIRD_PARTY_MODULES>", "", "^[~/]", "", "^[./]"],
+};
