4bcf9e547e2625f784d73c1f8a6b92146180a821
Tony Giorgio <tonygiorgio@protonmail.com>
2023-10-20 16:44:29 -0500

    Add simple nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.corepack_20
+            pkgs.nodejs_20
+            pkgs.python3
+            pkgs.just
+          ];
+        };
+      });
+}
