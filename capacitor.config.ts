ac45f5e8798022634eae2996fe6297e065ff1c57
Paul Miller <paul@paul.lol>
2023-11-03 15:34:41 -0500

    new mlkit barcode scanner

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,6 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.mutinywallet.mutinywallet',
+  backgroundColor: "171717",
   appName: 'Mutiny Wallet',
   webDir: 'dist/public',
   server: {
--- src/components/Reader.tsx
+++ src/components/Reader.tsx
@@ -1,10 +1,10 @@
-import { Capacitor } from "@capacitor/core";
 import {
     BarcodeFormat,
+    BarcodeScannedEvent,
     BarcodeScanner,
-    PermissionStates,
-    ScanResult
-} from "@mutinywallet/barcode-scanner";
+    PermissionStatus
+} from "@capacitor-mlkit/barcode-scanning";
+import { Capacitor } from "@capacitor/core";
 import QrScanner from "qr-scanner";
 import { onCleanup, onMount } from "solid-js";
 
@@ -18,26 +18,34 @@ export function Scanner(props: { onResult: (result: string) => void }) {
 
     const startScan = async () => {
         // Check camera permission
-        const permissions: PermissionStates =
+        const permissions: PermissionStatus =
             await BarcodeScanner.checkPermissions();
         if (permissions.camera === "granted") {
-            const callback = (result: ScanResult, err?: unknown) => {
-                if (err) {
-                    console.error(err);
-                    return;
+            await BarcodeScanner.startScan({ formats: [BarcodeFormat.QrCode] });
+
+            // The camera gets mounted behind everything so we need to make the background transparent
+            document.querySelector("html")?.classList.add("bg-transparent");
+
+            const listener = await BarcodeScanner.addListener(
+                "barcodeScanned",
+                // eslint-disable-next-line
+                async (result: BarcodeScannedEvent) => {
+                    document
+                        .querySelector("html")
+                        ?.classList.remove("bg-transparent");
+
+                    await BarcodeScanner.stopScan();
+                    await listener.remove();
+
+                    // if the result has content
+                    if (result && result.barcode) {
+                        handleResult({ data: result.barcode.rawValue }); // pass the raw scanned content
+                    }
                 }
-                // if the result has content
-                if (result && result.content) {
-                    handleResult({ data: result.content }); // pass the raw scanned content
-                }
-            };
-            await BarcodeScanner.start(
-                { formats: [BarcodeFormat.QR_CODE] },
-                callback
             );
         } else if (permissions.camera === "prompt") {
             // Request permission if it has not been asked before
-            const requestedPermissions: PermissionStates =
+            const requestedPermissions: PermissionStatus =
                 await BarcodeScanner.requestPermissions();
             if (requestedPermissions.camera === "granted") {
                 // If user grants permission, start the scan
@@ -50,8 +58,11 @@ export function Scanner(props: { onResult: (result: string) => void }) {
         }
     };
 
-    const stopScan = () => {
-        BarcodeScanner.stop();
+    const stopScan = async () => {
+        // Restore the background first to minimize flicker
+        document.querySelector("html")?.classList.remove("bg-transparent");
+        await BarcodeScanner.stopScan();
+        await BarcodeScanner.removeAllListeners();
     };
 
     onMount(async () => {
@@ -67,9 +78,9 @@ export function Scanner(props: { onResult: (result: string) => void }) {
         }
     });
 
-    onCleanup(() => {
+    onCleanup(async () => {
         if (Capacitor.isNativePlatform()) {
-            stopScan();
+            await stopScan();
         } else {
             scanner?.destroy();
         }
