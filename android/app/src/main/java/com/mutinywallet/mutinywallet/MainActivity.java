510281aa10d9dcd0c2bb110b4c9ecfa57b52ed4f
Paul Miller <paul@paul.lol>
2023-11-06 13:35:28 -0600

    get rid of android rubber band effect

--- android/app/src/main/java/com/mutinywallet/mutinywallet/MainActivity.java
+++ android/app/src/main/java/com/mutinywallet/mutinywallet/MainActivity.java
@@ -1,5 +1,20 @@
 package com.mutinywallet.mutinywallet;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        WebView webview = getBridge().getWebView();
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+    }
+}
