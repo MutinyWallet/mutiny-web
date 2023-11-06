efb8ce2b860a7bf95bc7af7b946144ac6fc412ac
Paul Miller <paul@paul.lol>
2023-11-06 13:35:28 -0600

    get rid of android rubber band effect

--- src/styles/dialogs.ts
+++ src/styles/dialogs.ts
@@ -1,3 +1,3 @@
 export const DIALOG_POSITIONER = "fixed inset-0 h-[100dvh] z-50";
 export const DIALOG_CONTENT =
-    "h-[100dvh] flex flex-col justify-between px-4 pt-4 pb-8 bg-m-grey-800 touch-manipulation select-none overflow-y-scroll disable-scrollbars";
+    "safe-top safe-bottom h-full flex flex-col justify-between px-4 pt-4 pb-8 bg-m-grey-800 touch-manipulation select-none overflow-y-scroll disable-scrollbars";
