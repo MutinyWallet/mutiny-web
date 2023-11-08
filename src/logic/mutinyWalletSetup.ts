88518a5adf1a3de32121a6116ab3de3c3e92fd9f
Paul Miller <paul@paul.lol>
2023-11-08 15:01:41 -0600

    ask browser for storage persistence

--- src/logic/mutinyWalletSetup.ts
+++ src/logic/mutinyWalletSetup.ts
@@ -197,6 +197,22 @@ export async function setupMutinyWallet(
 ): Promise<MutinyWallet> {
     console.log("Starting setup...");
 
+    // https://developer.mozilla.org/en-US/docs/Web/API/Storage_API
+    // Ask the browser to not clear storage
+    if (navigator.storage && navigator.storage.persist) {
+        navigator.storage.persist().then((persistent) => {
+            if (persistent) {
+                console.log(
+                    "Storage will not be cleared except by explicit user action"
+                );
+            } else {
+                console.log(
+                    "Storage may be cleared by the UA under storage pressure."
+                );
+            }
+        });
+    }
+
     const {
         network,
         proxy,
