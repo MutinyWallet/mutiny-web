ec3adb0cd4293b2c3880859b1e6d29314d263036
Paul Miller <paul@paul.lol>
2023-11-08 17:06:07 -0600

    show pending info when payment timed out

---
+++ src/components/successfail/MegaClock.tsx
@@ -0,0 +1,11 @@
+import megaclock from "~/assets/icons/megaclock.png";
+
+export function MegaClock() {
+    return (
+        <img
+            src={megaclock}
+            alt="fail"
+            class="mx-auto w-1/2 max-w-[30vh] flex-shrink"
+        />
+    );
+}
