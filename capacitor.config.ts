28b48aed4a2edecac2e51f2b79ae6be3271427aa
nk1tz <nate@unchained.com>
2024-01-12 17:21:14 -0600

    fix: apply prettier formatting consistently, mod script, readme typo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,13 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.mutinywallet.mutinywallet',
-  backgroundColor: "171717",
-  appName: 'Mutiny Wallet',
-  webDir: 'dist/public',
-  server: {
-    androidScheme: 'https'
-  }
+    appId: "com.mutinywallet.mutinywallet",
+    backgroundColor: "171717",
+    appName: "Mutiny Wallet",
+    webDir: "dist/public",
+    server: {
+        androidScheme: "https"
+    }
 };
 
 export default config;
