1f996cbdd7f4a34a6b1c9c43cdfec27e8186525a
benalleng <benalleng@gmail.com>
2024-02-18 22:56:03 -0500

    add translation key ci check

---
+++ tools/internal-rules/lib/index.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview internal eslint rules
+ * @author
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const requireIndex = require("requireindex");
+
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+
+// import all rules in lib/rules
+module.exports.rules = requireIndex(__dirname + "/rules");
