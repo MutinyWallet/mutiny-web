d34c822d34297905e87bf4e1764dbabe6927974f
benthecarman <benthecarman@live.com>
2024-02-21 10:42:31 +0000

    try waila from mutiny-node

--- src/logic/waila.ts
+++ src/logic/waila.ts
@@ -1,9 +1,6 @@
-import initWaila, { PaymentParams } from "@mutinywallet/waila-wasm";
+import { PaymentParams } from "@mutinywallet/mutiny-wasm";
 
 import { Result } from "~/utils";
-
-// Make sure we've initialzied waila before we try to use it
-await initWaila();
 
 export type ParsedParams = {
     original: string;
