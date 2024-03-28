c51626a575241c4006364d2ee6ed73abfd7efa63
Paul Miller <paul@paul.lol>
2024-03-28 12:37:36 -0500

    add delete nostr profile ability
    clear secure storage

--- src/components/SocialActionRow.tsx
+++ src/components/SocialActionRow.tsx
@@ -1,15 +1,16 @@
+import { TagItem } from "@mutinywallet/mutiny-wasm";
 import { cache, createAsync, useNavigate } from "@solidjs/router";
 import { Scan, Search } from "lucide-solid";
-import { For, Suspense } from "solid-js";
+import { createMemo, For, Suspense } from "solid-js";
 
-import { Circle, LabelCircle } from "~/components";
+import { Circle, LabelCircle, showToast } from "~/components";
 import { useMegaStore } from "~/state/megaStore";
 
 export function SocialActionRow(props: {
     onSearch: () => void;
     onScan: () => void;
 }) {
-    const [state, _actions] = useMegaStore();
+    const [state, actions] = useMegaStore();
     const navigate = useNavigate();
 
     const getContacts = cache(async () => {
@@ -23,6 +24,33 @@ export function SocialActionRow(props: {
     }, "contacts");
 
     const contacts = createAsync(() => getContacts(), { initialValue: [] });
+
+    const profileDeleted = createMemo(
+        () => state.mutiny_wallet?.get_nostr_profile().deleted
+    );
+
+    // TODO this is mostly copy pasted from chat, could be a shared util maybe
+    function sendToContact(contact?: TagItem) {
+        if (!contact) return;
+        const address = contact.ln_address || contact.lnurl;
+        if (address) {
+            actions.handleIncomingString(
+                (address || "").trim(),
+                (error) => {
+                    showToast(error);
+                },
+                (result) => {
+                    actions.setScanResult({
+                        ...result,
+                        contact_id: contact.id
+                    });
+                    navigate("/send");
+                }
+            );
+        } else {
+            console.error("no ln_address or lnurl");
+        }
+    }
 
     return (
         <div class="-ml-4 -mr-4 flex gap-2 overflow-x-scroll py-[1px] pl-4">
@@ -41,7 +69,11 @@ export function SocialActionRow(props: {
                             name={contact.name}
                             image_url={contact.primal_image_url}
                             onClick={() => {
-                                navigate(`/chat/${contact.id}`);
+                                if (profileDeleted()) {
+                                    sendToContact(contact);
+                                } else {
+                                    navigate(`/chat/${contact.id}`);
+                                }
                             }}
                         />
                     )}
--- src/routes/settings/NostrKeys.tsx
+++ src/routes/settings/NostrKeys.tsx
@@ -1,9 +1,12 @@
 import { A } from "@solidjs/router";
-import { Show } from "solid-js";
+import { SecureStoragePlugin } from "capacitor-secure-storage-plugin";
+import { Import, Trash } from "lucide-solid";
+import { createSignal, Match, Show, Switch } from "solid-js";
 import { QRCodeSVG } from "solid-qr-code";
 
 import {
     BackPop,
+    ConfirmDialog,
     DefaultMain,
     ExternalLink,
     FancyCard,
@@ -18,12 +21,58 @@ import {
 import { useI18n } from "~/i18n/context";
 import { useMegaStore } from "~/state/megaStore";
 
+function DeleteAccount() {
+    const i18n = useI18n();
+    const [state, _actions] = useMegaStore();
+
+    async function confirmDelete() {
+        setConfirmOpen(true);
+    }
+
+    const [confirmOpen, setConfirmOpen] = createSignal(false);
+    const [confirmLoading, setConfirmLoading] = createSignal(false);
+
+    async function deleteNostrAccount() {
+        setConfirmLoading(true);
+        try {
+            await state.mutiny_wallet?.delete_profile();
+            // Remove the nsec from secure storage if it exists
+            await SecureStoragePlugin.clear();
+            window.location.href = "/";
+        } catch (e) {
+            console.error(e);
+        }
+        setConfirmLoading(false);
+    }
+
+    return (
+        <>
+            <button
+                class="flex w-full items-center justify-center gap-2 rounded-xl border border-white/10 bg-neutral-900 p-2 text-m-red  no-underline active:-mb-[1px] active:mt-[1px] active:opacity-70"
+                onClick={confirmDelete}
+            >
+                <Trash class="w-4" />
+                {i18n.t("settings.nostr_keys.delete_account")}
+            </button>
+            <ConfirmDialog
+                loading={confirmLoading()}
+                open={confirmOpen()}
+                onConfirm={deleteNostrAccount}
+                onCancel={() => setConfirmOpen(false)}
+            >
+                {i18n.t("settings.nostr_keys.delete_account_confirm")}
+            </ConfirmDialog>
+        </>
+    );
+}
+
 export function NostrKeys() {
     const i18n = useI18n();
     const [state, _actions] = useMegaStore();
 
     const npub = () => state.mutiny_wallet?.get_npub();
     const nsec = () => state.mutiny_wallet?.export_nsec();
+    const profile = () => state.mutiny_wallet?.get_nostr_profile();
 
     return (
         <MutinyWalletGuard>
@@ -36,33 +85,51 @@ export function NostrKeys() {
                         {i18n.t("settings.nostr_keys.learn_more")}
                     </ExternalLink>
                 </NiceP>
-                <FancyCard>
-                    <VStack>
-                        <div class="w-[10rem] self-center rounded bg-white p-[1rem]">
-                            <QRCodeSVG
-                                value={npub() || ""}
-                                class="h-full max-h-[256px] w-full"
-                            />
-                        </div>
-                        <KeyValue key="Public Key">
-                            <MiniStringShower text={npub() || ""} />
-                        </KeyValue>
-                        <Show when={nsec()}>
-                            <KeyValue key="Private Key">
-                                <MiniStringShower text={nsec() || ""} hide />
-                            </KeyValue>
-                            <p class="text-base italic text-m-grey-350">
-                                {i18n.t("settings.nostr_keys.warning")}
-                            </p>
-                        </Show>
-                    </VStack>
-                </FancyCard>
-                <A
-                    href="/settings/importprofile"
-                    class="self-center text-base font-normal text-m-grey-400"
-                >
-                    Import different nostr profile
-                </A>
+                <Switch>
+                    <Match when={profile() && !profile().deleted}>
+                        <FancyCard>
+                            <VStack>
+                                <div class="w-[10rem] self-center rounded bg-white p-[1rem]">
+                                    <QRCodeSVG
+                                        value={npub() || ""}
+                                        class="h-full max-h-[256px] w-full"
+                                    />
+                                </div>
+                                <KeyValue key="Public Key">
+                                    <MiniStringShower text={npub() || ""} />
+                                </KeyValue>
+                                <Show when={nsec()}>
+                                    <KeyValue key="Private Key">
+                                        <MiniStringShower
+                                            text={nsec() || ""}
+                                            hide
+                                        />
+                                    </KeyValue>
+                                    <p class="text-base italic text-m-grey-350">
+                                        {i18n.t("settings.nostr_keys.warning")}
+                                    </p>
+                                </Show>
+                            </VStack>
+                        </FancyCard>
+                        <A
+                            href="/settings/importprofile"
+                            class="flex w-full items-center justify-center gap-2 rounded-xl border border-white/10 bg-neutral-900 p-2 text-m-grey-400 no-underline active:-mb-[1px] active:mt-[1px] active:opacity-70"
+                        >
+                            <Import class="w-4" />
+                            {i18n.t("settings.nostr_keys.import_profile")}
+                        </A>
+                        <DeleteAccount />
+                    </Match>
+                    <Match when={profile() && profile().deleted}>
+                        <A
+                            href="/settings/importprofile"
+                            class="flex w-full items-center justify-center gap-2 rounded-xl border border-white/10 bg-neutral-900 p-2 text-m-grey-400 no-underline active:-mb-[1px] active:mt-[1px] active:opacity-70"
+                        >
+                            <Import class="w-4" />
+                            {i18n.t("settings.nostr_keys.import_profile")}
+                        </A>
+                    </Match>
+                </Switch>
             </DefaultMain>
             <NavBar activeTab="settings" />
         </MutinyWalletGuard>
