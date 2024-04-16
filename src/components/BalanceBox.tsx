24195badecec97ecee35dcb4e2f44908f16071f5
benthecarman <benthecarman@live.com>
2024-04-16 15:03:19 -0500

    Optional node manager

--- src/components/BalanceBox.tsx
+++ src/components/BalanceBox.tsx
@@ -1,6 +1,14 @@
 import { A, useNavigate } from "@solidjs/router";
-import { Shuffle, Users } from "lucide-solid";
-import { createMemo, Match, Show, Suspense, Switch } from "solid-js";
+import { Plus, Shuffle, Trash, Users } from "lucide-solid";
+import {
+    createMemo,
+    createResource,
+    createSignal,
+    Match,
+    Show,
+    Suspense,
+    Switch
+} from "solid-js";
 
 import {
     AmountFiat,
@@ -11,6 +19,7 @@ import {
     InfoBox,
     MediumHeader,
     NiceP,
+    SubtleButton,
     VStack
 } from "~/components";
 import { useI18n } from "~/i18n/context";
@@ -47,9 +56,13 @@ const STYLE =
     "px-2 py-1 rounded-xl text-sm flex gap-2 items-center font-semibold";
 
 export function BalanceBox(props: { loading?: boolean; small?: boolean }) {
-    const [state, _actions] = useMegaStore();
+    const [state, _actions, sw] = useMegaStore();
     const navigate = useNavigate();
     const i18n = useI18n();
+
+    const [nodeManagerLoading, setNodeManagerLoading] = createSignal(false);
+
+    const lightningBalance = () => state.balance?.lightning || 0n;
 
     const totalOnchain = createMemo(
         () =>
@@ -63,6 +76,35 @@ export function BalanceBox(props: { loading?: boolean; small?: boolean }) {
             (state.balance?.confirmed || 0n) +
             (state.balance?.unconfirmed || 0n)
     );
+
+    const [hasSelfCustody, { refetch }] = createResource(async () => {
+        // short circuit if we have a balance
+        if (totalOnchain() > 0 || state.balance?.lightning || 0n > 0n) {
+            return true;
+        }
+
+        // otherwise check if we have created a node
+        const nodes: string[] = await sw.list_nodes();
+        return nodes.length > 0;
+    });
+
+    const createNodeManager = async () => {
+        if (confirm("Pass this test:")) {
+            setNodeManagerLoading(true);
+            await sw.create_node_manager_if_needed();
+            await refetch();
+            setNodeManagerLoading(false);
+        }
+    };
+
+    const removeNodeManager = async () => {
+        if (confirm("Are you sure:")) {
+            setNodeManagerLoading(true);
+            await sw.remove_node_manager();
+            await refetch();
+            setNodeManagerLoading(false);
+        }
+    };
 
     return (
         <VStack>
@@ -131,81 +173,123 @@ export function BalanceBox(props: { loading?: boolean; small?: boolean }) {
                 </Match>
             </Switch>
             <MediumHeader>{i18n.t("profile.self_custody")}</MediumHeader>
-            <FancyCard>
-                <Show when={!props.loading} fallback={<LoadingShimmer />}>
-                    <Switch>
-                        <Match when={state.safe_mode}>
-                            <div class="flex flex-col gap-1">
-                                <InfoBox accent="red">
-                                    {i18n.t("common.error_safe_mode")}
-                                </InfoBox>
-                            </div>
-                        </Match>
-                        <Match when={true}>
-                            <div class="flex flex-col gap-1">
-                                <div class="text-2xl">
-                                    <AmountSats
-                                        amountSats={
-                                            state.balance?.lightning || 0
-                                        }
-                                        icon="lightning"
-                                        denominationSize="lg"
-                                    />
-                                </div>
-                                <div class="text-lg text-white/70">
-                                    <Suspense>
-                                        <AmountFiat
-                                            amountSats={
-                                                state.balance?.lightning || 0
+            <Suspense>
+                <Switch>
+                    <Match when={hasSelfCustody()}>
+                        <FancyCard>
+                            <Show
+                                when={!props.loading}
+                                fallback={<LoadingShimmer />}
+                            >
+                                <Switch>
+                                    <Match when={state.safe_mode}>
+                                        <div class="flex flex-col gap-1">
+                                            <InfoBox accent="red">
+                                                {i18n.t(
+                                                    "common.error_safe_mode"
+                                                )}
+                                            </InfoBox>
+                                        </div>
+                                    </Match>
+                                    <Match when={true}>
+                                        <div class="flex flex-col gap-1">
+                                            <div class="text-2xl">
+                                                <AmountSats
+                                                    amountSats={lightningBalance()}
+                                                    icon="lightning"
+                                                    denominationSize="lg"
+                                                />
+                                            </div>
+                                            <div class="text-lg text-white/70">
+                                                <Suspense>
+                                                    <AmountFiat
+                                                        amountSats={lightningBalance()}
+                                                        denominationSize="sm"
+                                                    />
+                                                </Suspense>
+                                            </div>
+                                        </div>
+                                    </Match>
+                                </Switch>
+                            </Show>
+                            <hr class="my-2 border-m-grey-750" />
+                            <Show
+                                when={!props.loading}
+                                fallback={<LoadingShimmer />}
+                            >
+                                <div class="flex justify-between">
+                                    <div class="flex flex-col gap-1">
+                                        <div class="text-2xl">
+                                            <AmountSats
+                                                amountSats={totalOnchain()}
+                                                icon="chain"
+                                                denominationSize="lg"
+                                            />
+                                        </div>
+                                        <div class="text-lg text-white/70">
+                                            <Suspense>
+                                                <AmountFiat
+                                                    amountSats={totalOnchain()}
+                                                    denominationSize="sm"
+                                                />
+                                            </Suspense>
+                                        </div>
+                                    </div>
+                                    <div class="flex flex-col items-end justify-between gap-1">
+                                        <Show
+                                            when={
+                                                state.balance?.unconfirmed != 0n
                                             }
-                                            denominationSize="sm"
-                                        />
-                                    </Suspense>
+                                        >
+                                            <Indicator>
+                                                {i18n.t("common.pending")}
+                                            </Indicator>
+                                        </Show>
+                                        <Show
+                                            when={
+                                                state.balance?.unconfirmed ===
+                                                0n
+                                            }
+                                        >
+                                            <div />
+                                        </Show>
+                                        <Show when={usableOnchain() > 0n}>
+                                            <div class="self-end justify-self-end">
+                                                <A href="/swap" class={STYLE}>
+                                                    <Shuffle class="h-6 w-6" />
+                                                </A>
+                                            </div>
+                                        </Show>
+                                    </div>
                                 </div>
-                            </div>
-                        </Match>
-                    </Switch>
-                </Show>
-                <hr class="my-2 border-m-grey-750" />
-                <Show when={!props.loading} fallback={<LoadingShimmer />}>
-                    <div class="flex justify-between">
-                        <div class="flex flex-col gap-1">
-                            <div class="text-2xl">
-                                <AmountSats
-                                    amountSats={totalOnchain()}
-                                    icon="chain"
-                                    denominationSize="lg"
-                                />
-                            </div>
-                            <div class="text-lg text-white/70">
-                                <Suspense>
-                                    <AmountFiat
-                                        amountSats={totalOnchain()}
-                                        denominationSize="sm"
-                                    />
-                                </Suspense>
-                            </div>
-                        </div>
-                        <div class="flex flex-col items-end justify-between gap-1">
-                            <Show when={state.balance?.unconfirmed != 0n}>
-                                <Indicator>
-                                    {i18n.t("common.pending")}
-                                </Indicator>
+                                <Show
+                                    when={
+                                        totalOnchain() === 0n &&
+                                        lightningBalance() === 0n &&
+                                        state.federations &&
+                                        state.federations.length
+                                    }
+                                >
+                                    <SubtleButton
+                                        onClick={removeNodeManager}
+                                        loading={nodeManagerLoading()}
+                                    >
+                                        <Trash class="h-4 w-4" />
+                                    </SubtleButton>
+                                </Show>
                             </Show>
-                            <Show when={state.balance?.unconfirmed === 0n}>
-                                <div />
-                            </Show>
-                            <Show when={usableOnchain() > 0n}>
-                                <div class="self-end justify-self-end">
-                                    <A href="/swap" class={STYLE}>
-                                        <Shuffle class="h-6 w-6" />
-                                    </A>
-                                </div>
-                            </Show>
-                        </div>
-                    </div>
-                </Show>
-            </FancyCard>
+                        </FancyCard>
+                    </Match>
+                    <Match when={true}>
+                        <SubtleButton
+                            onClick={createNodeManager}
+                            loading={nodeManagerLoading()}
+                        >
+                            <Plus class="h-4 w-4" />
+                        </SubtleButton>
+                    </Match>
+                </Switch>
+            </Suspense>
         </VStack>
     );
 }
