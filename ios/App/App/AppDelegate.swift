549a810dfdf772ac1f38a518b0fd65cce3df5ecf
Paul Miller <paul@paul.lol>
2024-06-10 11:14:45 -0500

    make ios webview inspectable by safari in release

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,6 +8,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 16.4, *) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                if let vc = self.window?.rootViewController as? CAPBridgeViewController {
+                    vc.bridgedWebView?.isInspectable = true;
+                }
+            }
+        }
+        
         return true
     }
 
